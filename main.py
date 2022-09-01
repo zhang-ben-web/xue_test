e5c8537a2223303a780bb0f64d910b50b7e52943
zhang-ben-web <79961628+zhang-ben-web@users.noreply.github.com>
2022-09-01 10:48:11 +0800

    Create main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  return weather['weather'], math.floor(weather['temp']), math.floor(weather['low'])
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -40,6 +40,18 @@ def get_words():
     return get_words()
   return words.json()['data']['text']
 
+def get_dujitang():
+  words = requests.get("https://api.shadiao.pro/du")
+  if words.status_code != 200:
+    return get_words()
+  return words.json()['data']['text']
+
+def get_pengyouquan():
+  words = requests.get("https://api.shadiao.pro/du")
+  if words.status_code != 200:
+    return get_words()
+  return words.json()['data']['text']
+
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
@@ -47,7 +59,16 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature, "color":get_random_color()},"love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()}}
+wea, temperature, min_temperature = get_weather()
+data = {
+  "weather":{"value":wea, "color":get_random_color()},
+  "temperature":{"value":temperature, "color":get_random_color()},
+  "min_temperature":{"value":min_temperature, "color":get_random_color()},
+  "love_days":{"value":get_count(), "color":get_random_color()},
+  "birthday_left":{"value":get_birthday(), "color":get_random_color()},
+  "words":{"value":get_words(), "color":get_random_color()},
+  "dujitang":{"value":get_dujitang(), "color":get_random_color()},
+  "pengyouquan":{"value":get_pengyouquan(), "color":get_random_color()},
+}
 res = wm.send_template(user_id, template_id, data)
 print(res)
